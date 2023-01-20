01c8d301814d953345bb4d9a8d70146aade9cf57
jenmwa <jenmwa@gmail.com>
2023-01-20 16:14:29 +0100

    :lipstick: take away unnecessarily code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
-<script setup lang='ts'>
-  import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+  import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <main>
     <p>GSAP SVG<br />animation project</p>
-    <div class='logos'>
-      <a href='https://vitejs.dev' target='_blank'>
-        <img src='/vite.svg' class='logo' alt='Vite logo' />
+    <div class="logos">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
       </a>
-      <a href='https://vuejs.org/' target='_blank'>
-        <img src='./assets/vue.svg' class='logo vue' alt='Vue logo' />
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
       </a>
     </div>
-    <div class='svg'>
+    <div class="svg">
       <HelloWorld />
     </div>
     <a href="https://github.com/jenmwa" target="_blank">
-      <img class='github-icon' src='/github-svgrepo-com.svg' />
-      <p class='name'>@jenmwa</p>
+      <img class="github-icon" src="/github-svgrepo-com.svg" />
+      <p class="name">@jenmwa</p>
     </a>
   </main>
-
-  <!-- CREATE A PLAY AGAIN BUTTON -->
-  <!-- <button>IGEN</button> -->
 </template>
 
 <style scoped>
@@ -53,8 +50,8 @@
 
   .github-icon:hover {
     scale: 1.1;
-    filter: invert(100%) sepia(34%) saturate(3434%) hue-rotate(185deg) brightness(101%) contrast(91%);
-
+    filter: invert(100%) sepia(34%) saturate(3434%) hue-rotate(185deg)
+      brightness(101%) contrast(91%);
   }
 
   @media (width >= 740px) {
@@ -64,5 +61,4 @@
       margin: auto;
     }
   }
-
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-// between //, your name of the the repo... ex github.com/jenmwa/todoList , write /todoList/
 export default defineConfig({
   plugins: [vue()],
   base: '/gsap/',
