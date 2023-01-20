f3b54817689de31b7b96dd475595c04c8e80ba98
jenmwa <jenmwa@gmail.com>
2023-01-20 15:40:23 +0100

    :lipstick: add css styling a & refactor code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
-<script setup lang="ts">
-  import HelloWorld from "./components/HelloWorld.vue";
+<script setup lang='ts'>
+  import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <main>
     <p>GSAP SVG<br />animation project</p>
-    <div class="logos">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <div class='logos'>
+      <a href='https://vitejs.dev' target='_blank'>
+        <img src='/vite.svg' class='logo' alt='Vite logo' />
       </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <a href='https://vuejs.org/' target='_blank'>
+        <img src='./assets/vue.svg' class='logo vue' alt='Vue logo' />
       </a>
     </div>
-    <div class="svg">
+    <div class='svg'>
       <HelloWorld />
     </div>
-    <img class="github-icon" src="/github-svgrepo-com.svg" />
-    <p class="name">@jenmwa</p>
+    <a href="https://github.com/jenmwa" target="_blank">
+      <img class='github-icon' src='/github-svgrepo-com.svg' />
+      <p class='name'>@jenmwa</p>
+    </a>
   </main>
 
   <!-- CREATE A PLAY AGAIN BUTTON -->
@@ -46,6 +48,13 @@
   .github-icon {
     width: 50px;
     margin-bottom: -20px;
+    filter: #2196f3;
+  }
+
+  .github-icon:hover {
+    scale: 1.1;
+    filter: invert(100%) sepia(34%) saturate(3434%) hue-rotate(185deg) brightness(101%) contrast(91%);
+
   }
 
   @media (width >= 740px) {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,20 +12,20 @@
     methods: {
       startDrop() {
       gsap.to('#o', {
-        staggering: 0.1,
+        stagger: 0.1,
         ease: 'bounce.out',
         y: 30,
       })
       },
       spin() {
         gsap.to('#globe', {
-        rotation: 360,
-        duration: 10,
-        ease: 'linear',
-        transformOrigin: 'center center',
-        repeat: -1,
-        repeatDelay:0,
-      })
+          rotation: 360,
+          duration: 10,
+          ease: 'linear',
+          transformOrigin: 'center center',
+          repeat: -1,
+          repeatDelay:0,
+        })
       },
       heart() {
         gsap.from('#globe', {
@@ -40,24 +40,24 @@
       },
     },
 		mounted() {
-		gsap.fromTo("#h", {delay: 0.5, opacity:0, x:-100, duration: 7, onComplete: this.spin }, {opacity:1, x:5});
+		gsap.fromTo('#h', {delay: 0.5, opacity:0, x:-100, duration: 7, onComplete: this.spin }, {opacity:1, x:5});
       //CREATE A TIMELINE INSTEAD?
-    gsap.to("#e", {delay: 0.4, duration: 2.5, ease: "elastic.out(1, 0.3)",y: -5, x: 2});
-    gsap.to("#l1", {delay: 0.55, duration: 3, ease: "elastic.out(1, 0.3)",y: -10, x: 3});
-    gsap.to("#l2", {delay: 0.65, duration: 2.5, ease: "elastic.out(1, 0.3)",y: -10, x: 4 });
-    gsap.to("#o", {delay: 0.75, duration: 2, ease: "elastic.out(1, 0.3)",y: -20, x: 2});
-    gsap.to("#o", {delay: 1.2, rotation: 60, duration: 2, ease: "bounce.out", transformOrigin: 'center center', onComplete: this.startDrop,},);
+    gsap.to('#e', {delay: 0.4, duration: 2.5, ease: 'elastic.out(1, 0.3)',y: -5, x: 2});
+    gsap.to('#l1', {delay: 0.55, duration: 3, ease: 'elastic.out(1, 0.3)',y: -10, x: 3});
+    gsap.to('#l2', {delay: 0.65, duration: 2.5, ease: 'elastic.out(1, 0.3)',y: -10, x: 4 });
+    gsap.to('#o', {delay: 0.75, duration: 2, ease: 'elastic.out(1, 0.3)',y: -20, x: 2});
+    gsap.to('#o', {delay: 1.2, rotation: 60, duration: 2, ease: 'bounce.out', transformOrigin: 'center center', onComplete: this.startDrop,},);
 
-    gsap.from('#globe', { scale: 0,  duration: 10, transformOrigin: 'center center', onComplete: this.heart});
+    gsap.from('#globe', {delay: 4, scale: 0,  duration: 10, transformOrigin: 'center center', onComplete: this.heart});
 
     gsap.from('#W, #R, #L, #D', {delay: 12, opacity:0, duration: 1, });
 
-    gsap.from('#grupp_world', 1, {delay: 12, fill: 'black', duration: 3.5,});
-    gsap.to("#grupp_world", 1, {delay: 14, fill: '#cddc39', duration: 3.5 },);
-    gsap.from("#grupp_world", 1, {delay: 15, fill: '#cddc39', duration: 3.5 },);
+    gsap.from('#grupp_world', {delay: 12, fill: 'black', duration: 3.5,});
+    gsap.to('#grupp_world', {delay: 13, fill: '#cddc39', duration: 7 },);
+    gsap.from('#grupp_world', {delay: 17, fill: '#cddc39', duration: 3.5 },);
 
-    gsap.fromTo("#L", 1, {delay: 16, fill: 'black', duration: 5 }, {delay: 17, fill: '#2196f3', duration: 3.5 },);
-    gsap.to("#D", {delay: 13.5, duration: 2.5, rotate:15, transformOrigin: 'end end',});
+    gsap.fromTo('#L', {delay: 21, fill: 'black', duration: 5 }, {delay: 17, fill: '#2196f3', duration: 3.5 },);
+    gsap.to('#D', {delay: 13.5, duration: 2.5, rotate:15, transformOrigin: 'end end',});
 
 		},
 	};
